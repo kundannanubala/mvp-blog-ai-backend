714bad51fdab1d7fa90fe318dededec47a7f184f
sreesha baraar <10152002ssb@gmail.com>
2024-10-30 16:56:28 +0530

    updated summary and scrape_results
    add the groq llm api key in .env

--- core/config.py
+++ core/config.py
@@ -1,9 +1,12 @@
 from pydantic_settings import BaseSettings
+import os
 
 
 class Settings(BaseSettings):
     MONGODB_URI: str
     MONGODB_NAME: str
+    GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY")
+    GROQ_API_KEY: str = os.getenv("GROQ_API_KEY")
 
     class Config:
         env_file = ".env"
